d6fe4d990af8c70653b333fc472055cb02a6220b
AlexanderMezhenskyi <alexmezhenskyi@gmail.com>
2019-09-01 11:00:30 +0300

    Updates

---
+++ precache-manifest.4301be6dd10f954cb05e89290864ceb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad861eec199d709461a2fca1f68c42c",
+    "url": "/peopleList/index.html"
+  },
+  {
+    "revision": "f59fa30fd6f3b9841102",
+    "url": "/peopleList/static/css/main.33c57249.chunk.css"
+  },
+  {
+    "revision": "2988485418eaf6d4394d",
+    "url": "/peopleList/static/js/2.57b02338.chunk.js"
+  },
+  {
+    "revision": "f59fa30fd6f3b9841102",
+    "url": "/peopleList/static/js/main.5c176910.chunk.js"
+  },
+  {
+    "revision": "4dcd446174aa0a07af17",
+    "url": "/peopleList/static/js/runtime~main.f275de2c.js"
+  }
+]);
